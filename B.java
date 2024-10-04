709cee4f1df983d795d212aee4f9f2066378f426
santosh ajabe <santoshajabe2003@gmail.com>
2024-10-04 23:52:40 +0530

    all commit

---
+++ B.java
@@ -0,0 +1,12 @@
+class B
+{
+    public static void main(String[] args) {
+        
+        System.out.println("xyz");
+
+        System.out.println("pqr");
+
+
+        System.out.println(p);
+    }
+}
