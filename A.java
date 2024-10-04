c94e617192877eef670270f49233cc30d0d73710
santosh ajabe <santoshajabe2003@gmail.com>
2024-10-04 21:59:45 +0530

    this is first commit

---
+++ A.java
@@ -0,0 +1,25 @@
+public class A
+{
+    public void sum(){
+    int a =4;
+
+    int b=5;
+
+    int c= a+b;
+
+    System.out.println(c);
+
+    }
+    public static void main(String[] args) {
+        
+
+        System.out.println("ajabe");
+
+        A b = new A();
+        b.sum();
+
+    }
+
+   
+    
+}
