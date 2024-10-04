30e2c60e12a85a0e1388d53f243ce49c3c589438
santosh ajabe <santoshajabe2003@gmail.com>
2024-10-04 22:09:49 +0530

    this is first commit

---
+++ C.java
@@ -0,0 +1,22 @@
+public class C{
+
+    public static void main(String[] args) {
+        
+        String name;
+
+        int id;
+
+        long no;
+
+        name="xyz";
+
+        id=102;
+
+        no=23545454;
+
+        System.out.println(no);
+
+        System.out.println(id);
+
+    }
+}
